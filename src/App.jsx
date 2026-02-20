fd6c2d9a61467db71fa3adb2996147045e4cd537
Sneha Agarwal <snehaagarwal1322@gmail.com>
2026-02-20 23:19:45 +0530

    clean slate repo and start afresh from here now

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import ProtectedRoute from "./components/common/ProtectedRoute";
-import PublicRoute from "./components/common/PublicRoute";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <AuthProvider>
-        <ThemeProvider>
-          <Routes>
-            {/* Public routes - redirect authenticated users to dashboard */}
-            <Route
-              path="/"
-              element={
-                <PublicRoute>
-                  <Landing />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <PublicRoute>
-                  <Login />
-                </PublicRoute>
-              }
-            />
-            {/* Truly public - portfolio viewable by anyone */}
-            <Route path="/portfolio/:username" element={<Portfolio />} />
-
-            {/* Protected routes */}
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/resume"
-              element={
-                <ProtectedRoute>
-                  <Resume />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* 404 */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </ThemeProvider>
-      </AuthProvider>
-    </Router>
+  <></>
   );
 }
 
